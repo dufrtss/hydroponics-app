4f47e72ebb52c3d84df773f946f6531c15cb71ac
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-28 19:52:48 -0300

    added eslint and tailwind to the project

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default tseslint.config(
   {
@@ -13,7 +16,10 @@ export default tseslint.config(
     ]
   },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -22,22 +28,19 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      // '@typescript-eslint/no-explicit-any': 'off',
-      // '@typescript-eslint/no-floating-promises': 'warn',
-      // '@typescript-eslint/no-unsafe-assignment': 'off',
-      // '@typescript-eslint/no-unsafe-argument': 'off',
-      // '@typescript-eslint/no-unsafe-call': 'off',
-      // '@typescript-eslint/no-unsafe-member-access': 'off',
-      // '@typescript-eslint/no-unsafe-return': 'off',
-      // '@typescript-eslint/require-await': 'warn',
+
       'quotes': ['error', 'single', { avoidEscape: true }],
       'semi': ['error', 'never'],
-      "indent": ["error", 4],
-      "linebreak-style": ["error", "unix"]
+      'indent': ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      
+      'simple-import-sort/imports': 'error'
     }
   }
 )
