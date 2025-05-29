daaf479580a0ebb05af21306fdb4d3c5299bbd5b
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-28 22:02:43 -0300

    added more visuals to the layout and sign-in pages

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,33 @@
+import { Leaf } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className='min-h-screen grid grid-cols-2'>
+      <div className='h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between'>
+        <div className='flex items-center gap-3 text-lg font-medium text-foreground'>
+          <Leaf className='h-5 w-5' />
+          <span className='font-semibold'>Integrated Hydroponics Monitoring System</span>
+        </div>
+        
+        <footer className='flex flex-col text-sm'>
+          <span>
+            Made with ♥  by
+            <a
+              href='https://github.com/dufrtss'
+              target='_blank'
+              className='hover:underline'> Eduardo Freitas </a>
+            &
+            <a
+              href='https://github.com/SofiaMartinsLv'
+              target='_blank'
+              className='hover:underline'> Sofia Martins</a>
+          </span>
+          <span> Monitoring Panel &copy; Integrated Hydroponics Monitoring System - {new Date().getFullYear()}</span>
+        </footer>
+      </div>
 
-      <div>
+      <div className='flex flex-col items-center justify-center'>
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,38 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <title>Hydroponics | Login</title>
-      <h1>SignIn</h1>
+      
+      <div className='p-8'>
+        <div className='w-[350px] flex flex-col justify-center gap-6'>
+          <div className='flex flex-col gap-2 text-center'>
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Access Panel
+            </h1>
+
+            <p className='text-sm text-muted-foreground'>
+              Monitor your Hydroponics Farm Conditions!
+            </p>
+          </div>
+
+          <form className='flex flex-col gap-4'>
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='email'>
+                Your e-mail
+              </Label>
+              <Input id='email' type='email' />
+            </div>
+
+            <Button className='w-full' type='submit'>
+              Access Panel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
