cef5fc5e6ff50820e71074ff27cdeaa9b75baa87
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-28 22:39:59 -0300

    added the sign-up page and more visuals

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -7,22 +7,28 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
+const signUpForm = z.object({
+  name: z.string(),
   email: z.string().email(),
   password: z.string()
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignUpForm = z.infer<typeof signUpForm>
 
-export function SignIn() {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+export function SignUp() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignUpForm>()
+  const navigate = useNavigate()
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleSignUp(data: SignUpForm) {
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
     
-      toast.success('Authenticated.', {
-        description: 'Enjoy your stay!'
+      toast.success('Account successfully created.', {
+        description: 'Start monitoring now.',
+        action: {
+          label: 'Login',
+          onClick: () => navigate('/sign-in')
+        }
       })
     } catch {
       toast.error('Invalid credentials.')
@@ -31,30 +37,35 @@ export function SignIn() {
 
   return (
     <>
-      <title>Hydroponics | Login</title>
+      <title>Hydroponics | Register</title>
       
       <div className='p-8'>
         <div>
           <Button variant='ghost' asChild className='absolute right-8 top-8'>
-            <Link to="/sign-up">
-              New account
+            <Link to="/sign-in">
+              Login
             </Link>
           </Button>
         </div>
-
+        
         <div className='w-[350px] flex flex-col justify-center gap-6'>
           <div className='flex flex-col gap-2 text-center'>
             <h1 className='text-2xl font-semibold tracking-tight'>
-              Access panel
+              Register now and start monitoring
             </h1>
 
             <p className='text-sm text-muted-foreground'>
-              Monitor your hydroponics farm conditions.
+              Your farm awaits you.
             </p>
           </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className='flex flex-col gap-4'>
+          <form onSubmit={handleSubmit(handleSignUp)} className='flex flex-col gap-4'>
             <div className='flex flex-col gap-2'>
+              <Label htmlFor='name'>
+                Name
+              </Label>
+              <Input id='name' type='text' {...register('name')} />
+
               <Label htmlFor='email'>
                 Email
               </Label>
@@ -67,8 +78,12 @@ export function SignIn() {
             </div>
 
             <Button disabled={isSubmitting} className='w-full' type='submit'>
-              Access panel
+              Register account
             </Button>
+
+            <p className='px-6 text-center text-sm leading-relaxed text-muted-foreground'>
+              Upon registration, you agree with our <a className="underline underline-offset-4">terms of service</a> and <a className="underline underline-offset-4">privacy politics</a>.
+            </p>
           </form>
         </div>
       </div>
