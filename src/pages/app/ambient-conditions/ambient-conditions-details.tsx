cfe1e85bcf9980b2744fdb9dc1335e9f219424b5
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-30 22:14:58 -0300

    added ambient conditions details

---
+++ src/pages/app/ambient-conditions/ambient-conditions-details.tsx
@@ -0,0 +1,40 @@
+import { DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+
+export function AmbientConditionsDetais() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Sensor: DHT22</DialogTitle>
+        <DialogDescription>Ambient condition details</DialogDescription>
+      </DialogHeader>
+
+      <div className='flex flex-col py-2'>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className='text-muted-foreground'>Temperature</TableCell>
+              <TableCell className='flex justify-end'>
+                <div className='flex items-center gap-2'>
+                  <span>32.23</span>
+                  <span>°C</span>
+                </div>
+              </TableCell>
+              <TableCell className='font-mono font-medium'></TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className='text-muted-foreground'>Received at</TableCell>
+              <TableCell className='flex justify-end'>
+                <div className='flex items-center gap-2'>
+                  <span>15 minutes ago</span>
+                </div>
+              </TableCell>
+              <TableCell className='font-mono font-medium'></TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
--- src/pages/app/ambient-conditions/ambient-conditions-table-row.tsx
+++ src/pages/app/ambient-conditions/ambient-conditions-table-row.tsx
@@ -1,24 +1,32 @@
 import { Pencil, Search, Trash } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { TableCell, TableRow } from '@/components/ui/table'
+
+import { AmbientConditionsDetais } from './ambient-conditions-details'
 
 export function AmbientConditionsTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant='outline' size='sm'>
-          <Search className='h-3 w-3' />
-          <span className='sr-only'>Measurement details</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant='outline' size='sm'>
+              <Search className='h-3 w-3' />
+              <span className='sr-only'>Measurement details</span>
+            </Button>
+          </DialogTrigger>
+
+          <AmbientConditionsDetais />
+        </Dialog>
       </TableCell>
       <TableCell className='font-mono text-xs font-medium'>031093102s989fd8</TableCell>
       <TableCell className='font-mono font-medium'>32.23</TableCell>
       <TableCell className='font-mono font-medium'>°C</TableCell>
       <TableCell className='font-medium'>Temperature</TableCell>
       <TableCell className='font-medium'>DHT22</TableCell>
-      <TableCell className='font-medium'>ha 15 minutos
-      </TableCell>
+      <TableCell className='font-medium'>15 minutes ago</TableCell>
       {/* <TableCell>
         <Button variant='ghost' size='sm'>
           <Pencil className='mr-2 h-3 w-3' />
