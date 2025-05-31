bf5a83c18ba0d87b8a7ac5c236f4fc96160eb4bd
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-05-30 21:58:00 -0300

    added pagination component

--- src/pages/app/ambient-conditions/ambient-conditions.tsx
+++ src/pages/app/ambient-conditions/ambient-conditions.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from '@/components/pagination'
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 import { AmbientConditionsTableFilter } from './ambient-conditions-table-filter'
@@ -10,35 +11,38 @@ export function AmbientConditions() {
 
       <div className='flex flex-col gap-4'>
         <h1 className='text-3xl font-bold tracking-tight'>Ambient conditions</h1>
-      </div>
+      
 
-      <div className='flex flex-col gap-3'>
-        <AmbientConditionsTableFilter />
+        <div className='flex flex-col gap-3'>
+          <AmbientConditionsTableFilter />
 
-        <div className='border rounded-md'>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className='w-[64px]'></TableHead>
-                <TableHead className='w-[160px]'>Identifier</TableHead>
-                <TableHead className='w-[140px]'>Measurement</TableHead>
-                <TableHead className='w-[64px]'>Unit</TableHead>
-                <TableHead className='w-[200px]'>Type</TableHead>
-                <TableHead className='w-[200px]'>Sensor</TableHead>
-                <TableHead className='w-[160px]'>Received at</TableHead>
-                <TableHead className='w-[120px]'></TableHead>
-                <TableHead className='w-[120px]'></TableHead>
-              </TableRow>
-            </TableHeader>
+          <div className='border rounded-md'>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className='w-[64px]'></TableHead>
+                  <TableHead className='w-[160px]'>Identifier</TableHead>
+                  <TableHead className='w-[140px]'>Measurement</TableHead>
+                  <TableHead className='w-[64px]'>Unit</TableHead>
+                  <TableHead className='w-[200px]'>Type</TableHead>
+                  <TableHead className='w-[200px]'>Sensor</TableHead>
+                  <TableHead className='w-[160px]'>Received at</TableHead>
+                  <TableHead className='w-[120px]'></TableHead>
+                  <TableHead className='w-[120px]'></TableHead>
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return (
-                  <AmbientConditionsTableRow key={i} />
-                )
-              })}
-            </TableBody>
-          </Table>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return (
+                    <AmbientConditionsTableRow key={i} />
+                  )
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination page={1} totalCount={105} pageSize={10} />
         </div>
       </div>
     </>
