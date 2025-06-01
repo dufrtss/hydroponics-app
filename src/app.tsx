78ed5e3eafb8f16d8a0775ef8e6f642453d8689e
Eduardo Freitas <eduardofragadefreitas@gmail.com>
2025-06-01 12:57:22 -0300

    added api client, env variables parsing and react query to handle requests

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,20 @@
 import './styles/global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './router'
 
-export default function App() {
+export function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="hydroponics-theme">
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
